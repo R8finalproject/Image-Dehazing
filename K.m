3851e85bb881467025872533c14a2e21592051e8
R8finalproject <125950749+R8finalproject@users.noreply.github.com>
2023-03-08 10:55:15 +0530

    Update K.m

--- K.m
+++ K.m
@@ -1,4 +1,4 @@
-function f=K(x,a,Mean_scene,~,M,C)
+function f=K(x,a,Mean_scene,sourcePic,M,C)
 J_scene=x*(Mean_scene-min(min(Mean_scene)));
 A(1)=a*C(1);A(2)=C(2)*a;A(3)=C(3)*a;
 t=J_scene-A/(A*(x-(J_scene-min(min(J_scene)-0.9)));
