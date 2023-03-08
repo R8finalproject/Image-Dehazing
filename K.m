5c0076c57884d47bb6911d701a133186e769aee3
R8finalproject <125950749+R8finalproject@users.noreply.github.com>
2023-03-08 10:52:32 +0530

    Update K.m

--- K.m
+++ K.m
@@ -1,9 +1,10 @@
-function f=GS_s_K(k_determined,a,Mean_scene,~,T,C)
+function f=K(x,a,Mean_scene,~,M,C)
+J_scene=x*(Mean_scene-min(min(Mean_scene)));
 A(1)=a*C(1);A(2)=C(2)*a;A(3)=C(3)*a;
-t=Mean_scene-A/(A*(k_determined-(Mean_scene-min(min(Mean_scene)-0.9)));
-J(:,:,1) = sourcePic(:,:,1) - (1-t_).*A(1))./t_)/A(1);   
-J(:,:,2) = sourcePic(:,:,2) - (1-t_).*A(2))./t_)/A(2);  
-J(:,:,3) = sourcePic(:,:,3) - (1-t_).*A(3))./t_)/A(3);  
+t=J_scene-A/(A*(x-(J_scene-min(min(J_scene)-0.9)));
+J(:,:,1) = sourcePic(:,:,1) - (1-t).*A(1))./t)/A(1);   
+J(:,:,2) = sourcePic(:,:,2) - (1-t).*A(2))./t)/A(2);  
+J(:,:,3) = sourcePic(:,:,3) - (1-t).*A(3))./t)/A(3);  
 av=(J(:,:,1)+J(:,:,2)+J(:,:,3))/3;
-f=abs(mean2(av)-T);
+f=abs(mean2(av)-M);
 end
